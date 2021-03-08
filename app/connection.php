d638f6bb1de46441233263b2d76fce1b5e42a125
jonachen1005 <80160335+jonachen1005@users.noreply.github.com>
2021-03-08 23:12:53 +0800

    upload connection

---
+++ app/connection.php
@@ -0,0 +1,5 @@
+<?php
+    $con=mysqli_connect("db.sice.indiana.edu","i494f19_team62","my+sql=i494f19_team62", "i494f19_team62");
+if (!$con)
+	{die("Failed to connect to MySQL: " . mysqli_connect_error()); }
+?>
